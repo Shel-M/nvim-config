421cf8fb8d7917a995d18084f3214df5b5869cba
Sheldon M. <blackshadow606@yahoo.com>
2024-07-20 11:17:43 -0600

    Updating deprecated which-key config

--- lua/custom/plugins/config.lua
+++ lua/custom/plugins/config.lua
@@ -154,15 +154,15 @@ require("mason-lspconfig").setup({
         handlers = { lsp_zero.default_setup },
 })
 
-require("which-key").register {
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-        ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-}
+require("which-key").add({
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>d", group = "[D]ocument" },
+        { "<leader>g", group = "[G]it" },
+        { "<leader>h", group = "[H]arpoon" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>f", group = "[F]ind" },
+        { "<leader>w", group = "[W]orkspace" },
+})
 
 require("neodev").setup()
 
