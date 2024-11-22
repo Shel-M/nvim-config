96f1c444182b3139536468696cd1a381c4b9bc72
Sheldon M. <mcculloughs@partnercoloradocu.org>
2024-11-22 14:06:17 -0700

    Implementing workaround for nvim/rust-analyzer -32802 error.

--- lua/custom/plugins/config.lua
+++ lua/custom/plugins/config.lua
@@ -96,6 +96,17 @@ require("mason-lspconfig").setup({
                 end
         },
 })
+-- Workaround for https://github.com/neovim/neovim/issues/30985
+-- Can be removed once that issue is resolved.
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+                if err ~= nil and err.code == -32802 then
+                        return
+                end
+                return default_diagnostic_handler(err, result, context, config)
+        end
+end
 
 require("which-key").add(
         {
