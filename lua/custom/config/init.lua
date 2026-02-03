8806a44e18abfd30219f32b0102dbd6473c1d7b4
SheldonM <mcculloughs@partnercoloradocu.org>
2026-02-03 08:30:40 -0700

    Merging: Adding Neovide support

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -1,3 +1,7 @@
 require("custom/config/settings")
 require("custom/config/keymap")
 require("custom/config/commands")
+
+if vim.g.neovide then
+	require("custom/config/neovide")
+end
---
+++ lua/custom/config/neovide.lua
@@ -0,0 +1,4 @@
+vim.o.guifont = "CaskaydiaCove Nerd Font Mono"
+
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_cursor_animation_length = 0.0
