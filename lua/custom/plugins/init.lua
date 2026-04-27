448e0297da00a0a0ca13bf1c75c7df4dda5ee495
Sheldon M. <mcculloughs@partnercoloradocu.org>
2026-04-27 14:36:58 -0600

    Updating plugins, merging unpulled changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,10 @@ require("lazy").setup({
 	-- Simple setups
 
 	"tpope/vim-fugitive", -- Git integration with vim
-	"tpope/vim-rhubarb",  -- Github integration for vim-fugitive
+	"tpope/vim-rhubarb", -- Github integration for vim-fugitive
 	"NMAC427/guess-indent.nvim", -- Automatic tab and space detection
 	"folke/which-key.nvim", -- Keybind display / helper
-	"norcalli/nvim-colorizer.lua",
+	-- "norcalli/nvim-colorizer.lua",
 	"ThePrimeagen/harpoon",
 	"lewis6991/gitsigns.nvim", -- Adds git related signs to the gutter, as well as utilities for managing changes
 
@@ -18,14 +18,17 @@ require("lazy").setup({
 	-- Autocompletion
 	{
 		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = {
+			"saghen/blink.lib",
+			"rafamadriz/friendly-snippets",
+		},
 		-- See: https://cmp.saghen.dev/installation for opts explanations
 		opts = {
 			keymap = { preset = "enter" },
 			appearance = { nerd_font_variant = "mono" },
 			completion = { documentation = { auto_show = true } },
 			sources = {
-				default = { "lsp", "path", "snippets", "lazydev" },
+				default = { "lsp", "path", "snippets", "lazydev", "buffer" },
 				providers = {
 					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 				},
@@ -42,10 +45,10 @@ require("lazy").setup({
 	-- Status bar (uses theme)
 	{
 		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			options = {
 				icons_enabled = true,
-				theme = "catppuccin",
 				component_separators = "|",
 				section_separators = "",
 			},
@@ -62,15 +65,14 @@ require("lazy").setup({
 	-- Fuzzy finder
 	{
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
+		-- branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- I don't think I'm smart enough to understand quite what this does lol, but it's some async something for telescope
 			-- Fast C implementation of the fuzzy finder's algorithm (Requires `make` to compile)
 			{
 				-- The weird build commands and use of specifically Cmake is so that the plugin works on Windows, which I have to use for work. :(
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build =
-				"cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 				cond = function()
 					return vim.fn.executable("cmake") == 1
 				end,
@@ -86,9 +88,22 @@ require("lazy").setup({
 	-- Treesitter language parser
 	{
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
@@ -100,20 +115,75 @@ require("lazy").setup({
 			},
 			indent = { enable = true, disable = { "ruby" } },
 		},
-		config = function(_, opts)
-			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+		config = function()
+			-- ensure basic parser are installed
+			local parsers = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			}
+			require("nvim-treesitter").install(parsers)
 
-			-- Prefer git instead of curl in order to improve connectivity in some environments
-			require("nvim-treesitter.install").prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
+			---@param buf integer
+			---@param language string
+			local function treesitter_try_attach(buf, language)
+				-- check if parser exists and load it
+				if not vim.treesitter.language.add(language) then
+					return
+				end
+				-- enables syntax highlighting and other treesitter features
+				vim.treesitter.start(buf, language)
 
-			-- There are additional nvim-treesitter modules that you can use to interact
-			-- with nvim-treesitter. You should go explore a few and see what interests you:
-			--
-			--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-			--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+				-- enables treesitter based folds
+				-- for more info on folds see `:help folds`
+				-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+				-- vim.wo.foldmethod = 'expr'
+
+				-- check if treesitter indentation is available for this language, and if so enable it
+				-- in case there is no indent query, the indentexpr will fallback to the vim's built in one
+				local has_indent_query = vim.treesitter.query.get(language, "indents") ~= nil
+
+				-- enables treesitter based indentation
+				if has_indent_query then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end
+
+			local available_parsers = require("nvim-treesitter").get_available()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local buf, filetype = args.buf, args.match
+
+					local language = vim.treesitter.language.get_lang(filetype)
+					if not language then
+						return
+					end
+
+					local installed_parsers = require("nvim-treesitter").get_installed("parsers")
+
+					if vim.tbl_contains(installed_parsers, language) then
+						-- enable the parser if it is installed
+						treesitter_try_attach(buf, language)
+					elseif vim.tbl_contains(available_parsers, language) then
+						-- if a parser is available in `nvim-treesitter` auto install it, and enable it after the installation is done
+						require("nvim-treesitter").install(language):await(function()
+							treesitter_try_attach(buf, language)
+						end)
+					else
+						-- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+						treesitter_try_attach(buf, language)
+					end
+				end,
+			})
 		end,
 	},
 
@@ -130,7 +200,6 @@ require("lazy").setup({
 		-- end,
 	},
 
-
 	-- LSP confg
 	{
 		"neovim/nvim-lspconfig",
@@ -138,7 +207,7 @@ require("lazy").setup({
 			"mason-org/mason.nvim",
 			"mason-org/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			"j-hui/fidget.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
 			"saghen/blink.cmp",
 		},
 	},
